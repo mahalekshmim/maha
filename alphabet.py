7712f210cd72b75b8d8803175871741fd3f23c0e
mahalekshmim <41044027+mahalekshmim@users.noreply.github.com>
2019-01-07 15:28:25 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,9 @@
+print("Enter '0' for exit.");
+int = input("Enter any character: ");
+if int == '0':
+    exit();
+els
+    if((int>='a' and int<='z') or (int>='A' and int<='Z')):
+    	print(int, "is an alphabet.");
+    else
+    	print(int, "is not an alphabet.");
