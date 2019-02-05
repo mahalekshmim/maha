3334a084f85f905a39650afee092bb151522f3db
mahalekshmim <41044027+mahalekshmim@users.noreply.github.com>
2019-02-05 15:05:41 +0530

    Rename n exponent.py to number n exponent.py

---
+++ number n exponent.py
@@ -0,0 +1,13 @@
+def check Power (2,3)
+    if 2 <= 0 or 3 <=0
+        print "not a valid input";
+    else
+        for i in range (2,3)
+            x = **3i;
+            if x == 2 
+                print " True ";
+                print 3, "power ", i , "is", 2;
+                break;
+            elif x> 2
+                print "false"
+                break;
